0a8e6fd1766225a799da8860c0a4ee0b55252ea7
Katona Valentin <djvalee.yt@gmail.com>
2021-09-06 10:46:16 +0200

    valtozas

--- src/App.java
+++ src/App.java
@@ -7,13 +7,14 @@ public class App {
     public static void main(String[] args) {
         nevjegy();
         //Fejrész kiírása
-        System.out.println("Jelszavak");
+        System.out.println("Jelszavak program");
         //Verzió kiírása
         System.out.println("Verzió: 0.0.1");
         
         //Az a objektummal kérhetünk be a konzolról
         Scanner a = new Scanner(System.in);
-
+        System.out.println("  ");
+        System.out.println("Írja be az adatokat..");
         System.out.print("Felhasználónév: ");
         // A b változó tárolja a jelszót
         String b = a.nextLine();
@@ -26,22 +27,23 @@ public class App {
         try {
             /* 
             A jelszó, a felhasználónév és a 
-            használati helye a passList 
+            használati helye a passWord 
             objektumban van tárolva            
             */
-            Store passList = new Store(b, c, d);
+            Store passWord = new Store(b, c, d);
             FileWriter f = new FileWriter("pass.txt");
             PrintWriter pwr = new PrintWriter(f);
-            pwr.print(passList.user);
-            if(!passList.hollow()) { pwr.print(passList.retrieval()); }
-            pwr.print(passList.place);
+            pwr.print(passWord.user);
+            if(!passWord.hollow()) { pwr.print(passWord.retrieval()); }
+            pwr.print(passWord.place);
             pwr.close();
             iSiker = 1;
         } catch (IOException e) {
             System.err.println("Hiba! A fájlbaírás sikertelen. Keresse meg a fejlesztőt.");
         }
 
-        if(iSiker == 1) { System.out.println("Ok. A kiírás sikeres.");  }else {  System.out.println("Hiba! A kiírás sikertelen"); }
+        if(iSiker == 1) { System.out.println("Ok. A kiírás sikeres..");  }
+            else {  System.out.println("Hiba! A kiírás sikertelen"); }
 
     }
 
